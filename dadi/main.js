def38887a98abecbabec8c990e345cfd4771c568
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-21 16:34:25 +0200

    fixed iterazioni limitate email e massimizzato dadi

--- dadi/main.js
+++ dadi/main.js
@@ -1,26 +1,19 @@
 // "Gioco dei dadi" PC vs. Human: chiedi un numero all'utente tra 1 e 6 (giocata human) e estrai un numero casuale tra 1 e 6 (giocata PC). Chi fa il numero più alto vince!
 // chiedere all utente un numero da 1 a 6
-var dado_valore_human = prompt('Scegli un numero da 1 a 6 ?', 3);
-if (dado_valore_human <7 && dado_valore_human >=1 && isNaN(dado_valore_human) == false) {
-console.log (dado_valore_human);
+var dado_valore_human = parseInt(prompt('Scegli un numero da 1 a 6 ?'));
 //creare un  numero da 1 a 6 per il pc
-var dado_valore_pc = Math.floor(Math.random() * 10) + 1;
-if (dado_valore_pc < 7 ) {
+var dado_valore_pc = Math.floor(Math.random() * 5) + 1;
+if (dado_valore_human <7 && dado_valore_human >=1 && isNaN(dado_valore_human) == false) {
+    console.log (dado_valore_human);
     console.log (dado_valore_pc);
     // confrontare i due valori e vendere chi ha vinto
-if (dado_valore_human > dado_valore_pc) {
-    console.log ('Human vince!');
-}
-else if (dado_valore_human == dado_valore_pc) {
-    console.log ('Human e Pc pareggiano!');
-}
-else {
-    console.log ('Pc vince!');
-}
-}else {
-    console.log ('hai barato!! errore');
-}
-}
-else {
+    if (dado_valore_human > dado_valore_pc) {
+        console.log ('Human vince!');
+    } else if (dado_valore_human == dado_valore_pc) {
+        console.log ('Human e Pc pareggiano!');
+    } else {
+        console.log ('Pc vince!');
+    }
+} else {
     console.log ('hai barato!! errore');
 }
--- email/main.js
+++ email/main.js
@@ -3,14 +3,27 @@
 var mia_email = prompt('Quale è il tuo email ?', 'tomcruise@missionimpossible.com');
 
 // creare una lista di email
-var lista_email_vip = ['tomcruise@missionimpossible.com' ,'dicaprio@titanic.com' ,'ladygaga@badromance.com' ,'pippo@cartone.com' ,'picasso@guernica.com' ,'paint@disegno.com' ,'write@pencil.com']
+var lista_email_vip = ['tomcruise@missionimpossible.com' ,'dicaprio@titanic.com' ,'ladygaga@badromance.com' ,'pippo@cartone.com' ,'picasso@guernica.com' ,'paint@disegno.com' ,'write@pencil.com'];
 
 // verificare che l email è nella lista e dunque dare l output richiesto
- for (var i = 0; i < lista_email_vip.length; i++) {
-    if (mia_email != lista_email_vip[i]) {
-        console.log('non sei nella lista');
-    }   else{
-        console.log('sei in lista');}}
+// dichiaro la variabile di controllo
+var controllo;
+for(var i = 0; i < lista_email_vip.length; i++) {
+    if (mia_email == lista_email_vip[i]) {
+        controllo = true;
+    } else {
+        controllo = false;
+    }
+}
+
+// Faccio il controllo esteso per maggiore chiarezza
+if(controllo == true) {
+    //Sei in lista
+    console.log('sei nella lista');
+} else {
+    //Non sei in lista
+}
+
 // for (var i = 0; i < lista_email_vip.length || mia_email == lista_email_vip[i] ; i++) {
 //     console.log (mia_email);
 // }
