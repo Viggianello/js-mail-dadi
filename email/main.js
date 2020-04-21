3bd8c61dd60fc1a53e3f1b826f19bee5f588a8ba
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-21 14:07:34 +0200

    fixed file

--- email/main.js
+++ email/main.js
@@ -1,4 +1,4 @@
-// Chiedere all utente la sua email e verificare se tale è nella lista eamil 'vip', se in tal caso non lo fosse avvisarlo
+// "Sono in lista?" Chiedi all'utente la sua email controlla che sia nella lista di chi può accedere (ergo: la mail è presente in un array contenente le email "autorizzate") e stampa un messaggio appropriato (sei in lista/non sei in lista)
 // chiedere all utente la sua email
 var mia_email = prompt('Quale è il tuo email ?', 'tomcruise@missionimpossible.com');
 
@@ -6,9 +6,12 @@ var mia_email = prompt('Quale è il tuo email ?', 'tomcruise@missionimpossible.c
 var lista_email_vip = ['tomcruise@missionimpossible.com' ,'dicaprio@titanic.com' ,'ladygaga@badromance.com' ,'pippo@cartone.com' ,'picasso@guernica.com' ,'paint@disegno.com' ,'write@pencil.com']
 
 // verificare che l email è nella lista e dunque dare l output richiesto
-
-for (var i = 0; i < lista_email_vip.length || mia_email == lista_email_vip[i] ; i++) {
-    console.log (mia_email);
-}
-
-console.log (lista_email_vip);
+ for (var i = 0; i < lista_email_vip.length; i++) {
+    if (mia_email != lista_email_vip[i]) {
+        console.log ('Questa email non è nella lista');
+    } else {
+        console.log (mia_email);
+    }}
+// for (var i = 0; i < lista_email_vip.length || mia_email == lista_email_vip[i] ; i++) {
+//     console.log (mia_email);
+// }
